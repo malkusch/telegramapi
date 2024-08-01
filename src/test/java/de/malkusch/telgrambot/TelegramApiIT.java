a65fb5b0692f5e62d4634c1e0a352ffa197fcde9
Markus-Malkusch <markus.malkusch@ibm.com>
2024-08-01 11:43:22 +0200

    Add tests

--- src/test/java/de/malkusch/telgrambot/TelegramApiIT.java
+++ src/test/java/de/malkusch/telgrambot/TelegramApiIT.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 import static de.malkusch.telgrambot.TelegramApi.Reaction.THUMBS_UP;
 import static java.util.UUID.randomUUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TelegramApiIT {
 
@@ -42,6 +43,43 @@ public class TelegramApiIT {
         var id = telegram.send(api -> api.send(message));
 
         telegram.api.react(id, THUMBS_UP);
+    }
+
+    @Test
+    public void shouldPinMessage() {
+        var message = String.format("shouldPinMessage %s %s", LocalDateTime.now(), randomUUID());
+        var id = telegram.send(api -> api.send(message));
+
+        telegram.api.pin(id);
+
+        var pinned = telegram.api.pinned().get();
+        assertEquals(id, pinned.id());
+        assertEquals(message, pinned.message());
+    }
+
+    @Test
+    public void shouldUnpinMessage() {
+        var message = String.format("shouldUnpinMessage %s %s", LocalDateTime.now(), randomUUID());
+        var id = telegram.send(api -> api.send(message));
+        telegram.api.pin(id);
+
+        telegram.api.unpin(id);
+
+        var pinned = telegram.api.pinned();
+        assertFalse(pinned.isPresent());
+    }
+
+    @Test
+    public void shouldUnpinAllMessages() {
+        var message = String.format("shouldUnpinAllMessages %s %s", LocalDateTime.now(), randomUUID());
+        telegram.api.pin(telegram.send(api -> api.send(message)));
+        telegram.api.pin(telegram.send(api -> api.send(message)));
+        telegram.api.pin(telegram.send(api -> api.send(message)));
+
+        telegram.api.unpin();
+
+        var pinned = telegram.api.pinned();
+        assertFalse(pinned.isPresent());
     }
 
 }
