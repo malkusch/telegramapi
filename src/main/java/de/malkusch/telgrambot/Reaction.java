2f3d4c014610f946a6895f50edb2e603d7691a58
Markus-Malkusch <markus.malkusch@ibm.com>
2025-04-27 18:59:20 +0200

    Change THUMBS_UP reaction
    It appears Telegram changed the THUMPS_UP reaction emoji. It was
    👍 and now became \uD83D\uDC4D. Both seem to work now, but I
    have an application log with this error:
    
    > Sending to Telegram failed: [400] Bad Request: REACTION_INVALID
    
    See also https://core.telegram.org/bots/api#reactiontype

--- src/main/java/de/malkusch/telgrambot/Reaction.java
+++ src/main/java/de/malkusch/telgrambot/Reaction.java
@@ -1,6 +1,6 @@
 package de.malkusch.telgrambot;
 
 public record Reaction(String emoji) {
-    public static final Reaction THUMBS_UP = new Reaction("👍");
+    public static final Reaction THUMBS_UP = new Reaction("\uD83D\uDC4D");
     public static final Reaction UNKNOWN = new Reaction(null);
 }
